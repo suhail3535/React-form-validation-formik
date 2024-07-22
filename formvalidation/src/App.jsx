37e1336a99ec37203e794385d189b443e3617458
suhail khan <khansohail015@gmail.com>
2024-07-22 16:18:43 +0530

    cdn aos animation

---
+++ formvalidation/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+import { Signup } from "./component/Signup";
+import Data from "./component/Data";
+
+function App() {
+  return (
+    <>
+      {/* <Signup /> */}
+      <Data />
+    </>
+  );
+}
+
+export default App;
