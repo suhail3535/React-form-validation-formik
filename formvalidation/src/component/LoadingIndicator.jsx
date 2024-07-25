35d4adf1b5d0efce4cdd5bcf0b1582870cbb4ce1
suhail khan <khansohail015@gmail.com>
2024-07-25 15:00:42 +0530

    final submit

--- formvalidation/src/component/LoadingIndicator.jsx
+++ formvalidation/src/component/LoadingIndicator.jsx
@@ -3,10 +3,11 @@ import { Commet } from 'react-loading-indicators'; // Ensure this import is corr
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center ">
-      <div className="w-full max-w-xs m-auto  bg-white rounded-lg ">
-      <Commet color="rgb(49,46,129)" size="large" text="" textColor="" />
+    <div className="flex  items-center justify-center ">
+      <div className="w-full flex items-center justify-center max-w-xs m-auto  bg-white rounded-lg ">
+      <Commet color="rgb(49,46,129)" size="large" text="" textColor="" /><br/>
       </div>
+      
     </div>
   );
 };
